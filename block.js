587d7f63314f6faf2f130de6e91c66532aefa01a
Akashthe-coder <75618497+Akashthe-coder@users.noreply.github.com>
2021-03-31 18:38:45 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,23 @@
+class Block {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: false,
+          'restitution':0.8,
+            'friction':1.0,
+            'density':0.1
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("blue");
+      strokeWeight(2)
+      stroke("black")
+      rect(pos.x, pos.y, this.width, this.height);
+      
+    }
+  };
